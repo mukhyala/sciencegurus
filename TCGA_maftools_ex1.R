a0e933e63c10ae0f10aa3c21d8c8b7250beea30c
Kiran Mukhyala <mukhyala@gmail.com>
2018-07-02 20:13:34 -0700

    replace LUAD with PAAD

--- TCGA_maftools_ex1.R
+++ TCGA_maftools_ex1.R
@@ -1,9 +1,9 @@
 library(TCGAbiolinks)
-luad.maf <- GDCquery_Maf("LUAD", pipelines = "mutect2")
+luad.maf <- GDCquery_Maf("PAAD", pipelines = "mutect2")
 luad <- read.maf(maf=luad.maf)
 luad
 getSampleSummary(luad)
 getGeneSummary(luad)
 plotmafSummary(maf = luad, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
 oncoplot(maf = luad, top = 10, fontSize = 12)
-oncostrip(maf = luad, genes = c('KRAD','EGFR'))
+oncostrip(maf = luad, genes = c('KRAS','EGFR'))
