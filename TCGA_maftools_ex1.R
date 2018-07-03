b60af03ce0b3e0495ea7163e17d7cf1ad5923170
Kiran Mukhyala <mukhyala@gmail.com>
2018-07-02 19:54:54 -0700

    maftools example for TCGA data

---
+++ TCGA_maftools_ex1.R
@@ -0,0 +1,9 @@
+library(TCGAbiolinks)
+luad.maf <- GDCquery_Maf("LUAD", pipelines = "muse")
+luad <- read.maf(maf=luad.maf)
+luad
+getSampleSummary(luad)
+getGeneSummary(luad)
+plotmafSummary(maf = luad, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
+oncoplot(maf = luad, top = 10, fontSize = 12)
+oncostrip(maf = luad, genes = c('KRAD','EGFR'))
